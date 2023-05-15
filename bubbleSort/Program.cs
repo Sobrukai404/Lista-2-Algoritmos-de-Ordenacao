a9858127a69e4e914541d752b645a2abf8d62fcd
Sobrukai <lucas.lima6633@outlook.com>
2023-05-15 20:21:29 -0300

    Adicionar arquivos de projeto.

---
+++ bubbleSort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class BubbleSort
+{
+    static void Main()
+    {
+        int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+        Console.WriteLine("Array antes da ordenação:");
+        PrintArray(arr);
+
+        BubbleSortAlgorithm(arr);
+
+        Console.WriteLine("Array após a ordenação:");
+        PrintArray(arr);
+    }
+
+    static void BubbleSortAlgorithm(int[] arr)
+    {
+        int n = arr.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    // Troca os elementos se estiverem na ordem errada
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n; ++i)
+            Console.Write(arr[i] + " ");
+        Console.WriteLine();
+    }
+}
---
+++ selectionSort/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class SelectionSort
+{
+    static void Main()
+    {
+        int[] arr = { 64, 25, 12, 22, 11 };
+        Console.WriteLine("Array antes da ordenação:");
+        PrintArray(arr);
+
+        SelectionSortAlgorithm(arr);
+
+        Console.WriteLine("Array após a ordenação:");
+        PrintArray(arr);
+    }
+
+    static void SelectionSortAlgorithm(int[] arr)
+    {
+        int n = arr.Length;
+
+        // Itera através de todos os elementos do array
+        for (int i = 0; i < n - 1; i++)
+        {
+            // Encontra o elemento mínimo no array não ordenado
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++)
+            {
+                if (arr[j] < arr[minIndex])
+                    minIndex = j;
+            }
+
+            // Troca o elemento mínimo encontrado com o primeiro elemento não ordenado
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n; ++i)
+            Console.Write(arr[i] + " ");
+        Console.WriteLine();
+    }
+}
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
